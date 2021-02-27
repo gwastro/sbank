e84aa304693ff56b3ef8c7054f8bd872fdf00ad9
Ian Harry <ian.harry@ligo.org>
2021-02-27 15:49:36 +0000

    Add WIP draft of cython interface

---
+++ sbank/overlap.pyx
@@ -0,0 +1,33 @@
+# Copyright (C) 2021 Ian Harry
+
+import cython
+
+cdef extern from "overlap_lib.c":
+    ctypedef struct WS
+    WS *SBankCreateWorkspaceCache()
+    void SBankDestroyWorkspaceCache(WS *workspace_cache)
+    double _SBankComputeMatch(double complex *inj, double complex *tmplt, size_t min_len, double delta_f, WS *workspace_cache)
+    double _SBankComputeRealMatch(double complex *inj, double complex *tmplt, size_t min_len, double delta_f, WS *workspace_cache)
+    double _SBankComputeMatchMaxSkyLoc(double complex *hp, double complex *hc, const double hphccorr, double complex *proposal, size_t min_len, double delta_f, WS *workspace_cache1, WS *workspace_cache2)
+    double _SBankComputeMatchMaxSkyLocNoPhase(double complex *hp, double complex *hc, const double hphccorr, double complex *proposal, size_t min_len, double delta_f, WS *workspace_cache1, WS *workspace_cache2)
+
+
+# https://www.mail-archive.com/cython-dev@codespeak.net/msg06363.html
+cdef class SbankWorkspaceCache:
+    cdef WS *workspace
+
+    def __cinit__(self):
+        self.workspace = self.__create()
+        
+    def __dealloc__(self):
+        if self.workspace != NULL:
+            XLALDestroySBankWorkspaceCache(self.workspace)
+
+    cdef WS* __create(self):
+        cdef WS *temp
+        temp = XLALCreateSBankWorkspaceCache()
+        return temp
+
+    cdef WS* get_workspace(self):
+        return self.workspace
+
