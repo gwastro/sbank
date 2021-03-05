7c6a2c1769d7262aaf28d2a43b63e08ab4ddb2b0
Duncan Macleod <duncan.macleod@ligo.org>
2021-03-05 05:08:51 +0000

    setup.py: pedantic cleanup

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@
 
 import os
 import re
-import numpy
 from pathlib import Path
 
 from setuptools import setup, Extension
+
+import numpy
+
 from Cython.Build import cythonize
 
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
@@ -18,7 +20,7 @@ __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
 def find_version(path, varname="__version__"):
     """Parse the version metadata in the given file.
     """
-    with Path(path).open('r') as fobj:
+    with Path(path).open("r") as fobj:
         version_file = fobj.read()
     version_match = re.search(
         r"^{0} = ['\"]([^'\"]*)['\"]".format(varname),
@@ -30,30 +32,40 @@ def find_version(path, varname="__version__"):
     raise RuntimeError("Unable to find version string.")
 
 
-exts = []
-cython_compile_args = ['-O3', '-w', '-ffast-math',
-                       '-ffinite-math-only']
+# define cython options
+cython_compile_args = [
+    "-O3",
+    "-w",
+    "-ffast-math",
+    "-ffinite-math-only",
+]
 cython_directives = {
-    'embedsignature': True,
-    'language_level': 3,
+    "embedsignature": True,
+    "language_level": 3,
 }
-# if running in CI, enable coverage for cython
+
+# enable coverage for cython
 if int(os.getenv("CYTHON_LINETRACE", "0")):
     cython_directives["linetrace"] = True
     cython_compile_args.append("-DCYTHON_TRACE")
 
-ext = Extension("sbank.overlap_cpu",
-                ["sbank/overlap_cpu.pyx"],
-                include_dirs=[numpy.get_include()],
-                language='c',
-                libraries=['lal'],
-                extra_compile_args=cython_compile_args,
-                extra_link_args=[])
-exts.append(ext)
+# define compiled extensions
+exts = [
+    Extension(
+        "sbank.overlap_cpu",
+        ["sbank/overlap_cpu.pyx"],
+        include_dirs=[numpy.get_include()],
+        language="c",
+        libraries=["lal"],
+        extra_compile_args=cython_compile_args,
+        extra_link_args=[],
+    ),
+]
 
+# -- build the thing
 # this function only manually specifies things that aren't
 # supported by setup.cfg (as of setuptools-30.3.0)
 setup(
-    version=find_version(Path('sbank') / "__init__.py"),
+    version=find_version(Path("sbank") / "__init__.py"),
     ext_modules=cythonize(exts, compiler_directives=cython_directives),
 )
