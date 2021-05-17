e0eecc07780a3d383b3f38b643af92d1a6f2fee4
Ian Harry <ian.harry@ligo.org>
2021-05-17 15:14:27 +0100

    Try this(?)

--- tools/cibuildwheel_prep.sh
+++ tools/cibuildwheel_prep.sh
@@ -1,3 +1,3 @@
 pip install lalsuite
 cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.libs/lib*so* /usr/lib
-ln -s /usr/lib/liblal-*so* /usr/lib/liblal.so
+ln -sf /usr/lib/liblal-*so* /usr/lib/liblal.so
