b48bdfeba94748c5166a0a18324a973ab6e776d9
Ian Harry <ian.harry@ligo.org>
2021-05-17 15:18:27 +0100

    Debug

--- tools/cibuildwheel_prep.sh
+++ tools/cibuildwheel_prep.sh
@@ -1,3 +1,5 @@
 pip install lalsuite
 cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.libs/lib*so* /usr/lib
+echo /usr/lib/liblal-*so*
+ls /usr/lib/
 ln -sf /usr/lib/liblal-*so* /usr/lib/liblal.so
