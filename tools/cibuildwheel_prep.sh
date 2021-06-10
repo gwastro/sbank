a86759b4da068cb13ea6234ae9f44318f6945592
Ian Harry <ian.harry@ligo.org>
2021-06-10 20:54:07 +0100

    Update cibuildwheel_prep.sh

--- tools/cibuildwheel_prep.sh
+++ tools/cibuildwheel_prep.sh
@@ -11,6 +11,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     ln -sf `python -c 'import sys; print (sys.path[-1])'`/lalsuite.*libs/liblal-*so* /usr/lib/liblal.so
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac names are quite different
-    sudo cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/lib*dylib ${HOME}/lib
-    sudo cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/liblal.*.dylib ${HOME}/lib/liblal.dylib
+    mkdir -p ~/lib
+    cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/lib*dylib ~/lib
+    cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/liblal.*.dylib ~/lib/liblal.dylib
 fi # Don't consider anything else at present
