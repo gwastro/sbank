1ed3425c7a40a9710457ce43dc1c8bd710a6eb25
Ian Harry <ian.harry@ligo.org>
2021-06-10 14:57:59 +0100

    Update cibuildwheel_prep.sh

--- tools/cibuildwheel_prep.sh
+++ tools/cibuildwheel_prep.sh
@@ -2,9 +2,12 @@ set -e
 # This is used in github actions when building the wheels for distribution.
 # Do not run this outside of that!
 pip install lalsuite
-ls `python -c 'import sys; print (sys.path[-1])'`
-ls `python -c 'import sys; print (sys.path[-1])'`/lalsimulation/_lalsimulation*.so
-otool -L `python -c 'import sys; print (sys.path[-1])'`/lalsimulation/_lalsimulation*.so
-ls `python -c 'import sys; print (sys.path[-1])'`/lalsuite.*libs/
-cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.*libs/lib*so* /usr/lib
-ln -sf `python -c 'import sys; print (sys.path[-1])'`/lalsuite.*libs/liblal-*so* /usr/lib/liblal.so
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Needed for linux
+    cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.libs/lib*so* /usr/lib
+    ln -sf `python -c 'import sys; print (sys.path[-1])'`/lalsuite.*libs/liblal-*so* /usr/lib/liblal.so
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac names are quite different
+    cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/lib*dylib /usr/lib
+    ln -sf `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/liblal.*.dylib /usr/lib/liblal.dylib
