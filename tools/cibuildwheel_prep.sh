eaf941a6493592bb53c8581932bb5d2375bb4552
Ian Harry <ian.harry@ligo.org>
2021-06-10 11:47:52 +0100

    Update cibuildwheel_prep.sh

--- tools/cibuildwheel_prep.sh
+++ tools/cibuildwheel_prep.sh
@@ -1,9 +1,9 @@
 set -e
 # This is used in github actions when building the wheels for distribution.
 # Do not run this outside of that!
-pip install --upgrade pip
+python -m pip install --upgrade pip
 echo `python --version`
 echo `pip --version`
-pip install lalsuite
+python -m pip install lalsuite
 cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.libs/lib*so* /usr/lib
 ln -sf `python -c 'import sys; print (sys.path[-1])'`/lalsuite.libs/liblal-*so* /usr/lib/liblal.so
