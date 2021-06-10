927d7f30cfec184a018ab52a36cae46ff7548db4
Ian Harry <ian.harry@ligo.org>
2021-06-10 14:20:31 +0100

    Update cibuildwheel_prep.sh

--- tools/cibuildwheel_prep.sh
+++ tools/cibuildwheel_prep.sh
@@ -3,8 +3,8 @@ set -e
 # Do not run this outside of that!
 pip install lalsuite
 ls `python -c 'import sys; print (sys.path[-1])'`
-ls `python -c 'import sys; print (sys.path[-1])'`/lalsimulation/_lalsimulation.so
-otool -L `python -c 'import sys; print (sys.path[-1])'`/lalsimulation/_lalsimulation.so
+ls `python -c 'import sys; print (sys.path[-1])'`/lalsimulation/_lalsimulation*.so
+otool -L `python -c 'import sys; print (sys.path[-1])'`/lalsimulation/_lalsimulation*.so
 ls `python -c 'import sys; print (sys.path[-1])'`/lalsuite.libs/
 cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.libs/lib*so* /usr/lib
 ln -sf `python -c 'import sys; print (sys.path[-1])'`/lalsuite.libs/liblal-*so* /usr/lib/liblal.so
