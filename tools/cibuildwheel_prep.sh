1da4c963764148fb7041a639ddf6742b9cf22d3b
Ian Harry <ian.harry@ligo.org>
2021-06-10 21:28:28 +0100

    Update cibuildwheel_prep.sh

--- tools/cibuildwheel_prep.sh
+++ tools/cibuildwheel_prep.sh
@@ -1,7 +1,7 @@
 set -e
 # This is used in github actions when building the wheels for distribution.
 # DO NOT RUN THIS SCRIPT OUTSIDE OF THAT!!!
-pip install lalsuite
+#pip install lalsuite
 # DEBUG COMMAND BELOW. THIS MUST BE REMOVED
 pip install --upgrade git+https://github.com/spxiwh/delocate.git
 
@@ -11,7 +11,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     ln -sf `python -c 'import sys; print (sys.path[-1])'`/lalsuite.*libs/liblal-*so* /usr/lib/liblal.so
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac names are quite different
-    sudo echo "export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:`python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/" >> /etc/bashrc
+    sudo port install lal
+    #sudo echo "export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:`python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/" >> /etc/bashrc
     #sudo cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/lib*dylib /usr/local/lib
     #sudo cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/liblal.*.dylib /usr/local/lib/liblal.dylib
 
