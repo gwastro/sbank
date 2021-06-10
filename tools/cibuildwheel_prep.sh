eaf34d6e528862daf349b6d6d947b505a72f923c
Ian Harry <ian.harry@ligo.org>
2021-06-10 15:28:48 +0100

    Update cibuildwheel_prep.sh

--- tools/cibuildwheel_prep.sh
+++ tools/cibuildwheel_prep.sh
@@ -1,6 +1,6 @@
 set -e
 # This is used in github actions when building the wheels for distribution.
-# Do not run this outside of that!
+# DO NOT RUN THIS SCRIPT OUTSIDE OF THAT!!!
 pip install lalsuite
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -9,6 +9,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     ln -sf `python -c 'import sys; print (sys.path[-1])'`/lalsuite.*libs/liblal-*so* /usr/lib/liblal.so
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac names are quite different
-    cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/lib*dylib /usr/lib
-    ln -sf `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/liblal.*.dylib /usr/lib/liblal.dylib
+    sudo cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/lib*dylib /usr/lib
+    sudo ln -sf `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/liblal.*.dylib /usr/lib/liblal.dylib
 fi # Don't consider anything else at present
