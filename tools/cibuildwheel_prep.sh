f47feeb562c422090c8ffb52ccbc20782688c904
Ian Harry <ian.harry@ligo.org>
2021-06-10 11:34:19 +0100

    Update cibuildwheel_prep.sh
    Debugging. lalsuite *should* be seen, but is not??

--- tools/cibuildwheel_prep.sh
+++ tools/cibuildwheel_prep.sh
@@ -1,6 +1,8 @@
 set -e
 # This is used in github actions when building the wheels for distribution.
 # Do not run this outside of that!
+echo `python --version`
+echo `pip --version`
 pip install lalsuite
 cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.libs/lib*so* /usr/lib
 ln -sf `python -c 'import sys; print (sys.path[-1])'`/lalsuite.libs/liblal-*so* /usr/lib/liblal.so
