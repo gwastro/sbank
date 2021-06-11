fa599cc61e3930609f7c93d6525f64570e0b98a8
Ian Harry <ian.harry@ligo.org>
2021-06-11 15:55:37 +0100

    Update cibuildwheel_prep.sh

--- tools/cibuildwheel_prep.sh
+++ tools/cibuildwheel_prep.sh
@@ -16,6 +16,8 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     #source ~/.bashrc
     #conda activate root
     #echo "conda activate root" >> ~/.bashrc
+    echo `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/
+    ls `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/
     export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:`python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/
     echo "export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:`python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/" >> ~/.bashrc 
     #sudo cp `python -c 'import sys; print (sys.path[-1])'`/lalsuite.dylibs/lib*dylib /usr/local/lib
