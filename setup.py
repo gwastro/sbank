6d155613ca1428b69652d5b5653290fa9e63259f
Ian Harry <ian.harry@ligo.org>
2024-04-26 12:01:06 -0700

    Use pkgconfig for lal dirs

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@
 """
 
 import os
+import subprocess
 
 from setuptools import setup, Extension
 
@@ -13,6 +14,15 @@ import numpy
 from Cython.Build import cythonize
 
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
+
+# Ensure we can find lal libraries
+def pkgconfig(package, kw):
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    output = subprocess.getoutput(
+        'pkg-config --cflags --libs {}'.format(package))
+    for token in output.strip().split():
+        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+    return kw
 
 
 # define cython options
@@ -33,16 +43,24 @@ if int(os.getenv("CYTHON_LINETRACE", "0")):
     cython_directives["linetrace"] = True
     cython_compile_args.append("-DCYTHON_TRACE")
 
+# Set extension arguments
+extension_kwargs = {
+    include_dirs=[numpy.get_include()],
+    language="c",
+    libraries=["lal"],
+    extra_compile_args=cython_compile_args,
+    extra_link_args=[],
+}
+
+# lal arguments
+extension_kwargs = pkgconfig('lal', extension_kwargs)
+
 # define compiled extensions
 exts = [
     Extension(
         "sbank.overlap_cpu",
         ["sbank/overlap_cpu.pyx"],
-        include_dirs=[numpy.get_include()],
-        language="c",
-        libraries=["lal"],
-        extra_compile_args=cython_compile_args,
-        extra_link_args=[],
+        **extension_kwargs
     ),
 ]
 
